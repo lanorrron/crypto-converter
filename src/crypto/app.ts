048e9efb8b63b3bc59542e024a1da7da29a1fc56
Lanor <lanorr.ron@gmail.com>
2025-04-04 15:43:54 -0400

    Implemented Singleton design pattern for obtaining AWS secrets

--- src/crypto/app.ts
+++ src/crypto/app.ts
@@ -1,27 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import axios from "axios";
-import { getSecret } from "../secrets/coingecko";
+import { SecretManager } from "../utils/secretManager";
 
 // list top 10 coins by marketcap
-
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const baseUrl = process.env.COINGECKO_BASE_URL;
+    const secreName = process.env.COINGECKO_SECRET_NAME;
 
     try {
-        const {API_KEY} = await getSecret();
+        if(!secreName) throw new Error("SecretName not found")
+
+        const test = SecretManager.getInstance();
+        const secret = await test.getSecret<{API_KEY:string}>(secreName)
+
 
         const url = `${baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10`
         const response:any = await axios.get(url,{
             headers:{
                 'Accept': 'application/json',
-                'x-cg-demo-api-key': API_KEY
+                'x-cg-demo-api-key': secret.API_KEY
             }
         })
         return {
             statusCode: 200,
             body: JSON.stringify({
                 data:response.data,
-                apikey:API_KEY
+                apikey:secret.API_KEY
             }),
         };
     }
