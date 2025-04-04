6912efcc2987ac387fae7e46a7e8e65e3239d9fc
Lanor <lanorr.ron@gmail.com>
2025-04-04 16:16:55 -0400

    Lambda functions organized into modules

--- src/modules/coingecko/app.ts
+++ src/modules/coingecko/app.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import axios from "axios";
-import { SecretManager } from "../utils/secretManager";
+import { SecretManager } from "../../utils/secretManager";
 
 // list top 10 coins by marketcap
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -11,21 +11,20 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         if(!secreName) throw new Error("SecretName not found")
 
         const test = SecretManager.getInstance();
-        const secret = await test.getSecret<{API_KEY:string}>(secreName)
+        const secret = await test.getSecret<{api_key_coingecko:string}>(secreName)
 
 
         const url = `${baseUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10`
         const response:any = await axios.get(url,{
             headers:{
                 'Accept': 'application/json',
-                'x-cg-demo-api-key': secret.API_KEY
+                'x-cg-demo-api-key': secret.api_key_coingecko
             }
         })
         return {
             statusCode: 200,
             body: JSON.stringify({
                 data:response.data,
-                apikey:secret.API_KEY
             }),
         };
     }
