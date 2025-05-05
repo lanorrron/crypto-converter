ba3ab8b6ec33a6c30b60eac11583df81b188b6e3
Lanor <lanorr.ron@gmail.com>
2025-05-05 11:21:20 -0400

    added image to meta data

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,6 +16,33 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Sam Crypto',
   description: 'Information about crypto',
+  metadataBase: new URL('https://main.d26wywtdu2y5ih.amplifyapp.com'),
+  openGraph: {
+    type: 'website',
+    url: '',
+    title: 'Sam Crypto',
+    siteName: 'Sam Crypto',
+    description: 'Crypto converter and prices: losers, gainers, and popular coins.',
+    images: [
+      {
+        url: 'https://main.d26wywtdu2y5ih.amplifyapp.com/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'San crypto page',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Sam Crypto',
+    description: 'Crypto converter and prices: losers, gainers, and popular coins.',
+    images: 'https://main.d26wywtdu2y5ih.amplifyapp.com/og-image.png',
+  },
+  other: {
+    'og:image:secure_url': 'https://main.d26wywtdu2y5ih.amplifyapp.com/og-image.png',
+    'og:image:type': 'image/png',
+    'og:image:alt': 'Portada de Sam Crypto',
+  },
 };
 
 export default function RootLayout({
@@ -25,9 +52,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster />
       </body>
